abda572bb6d9831c418390bc5bebf6a1f576d277
Robson Mendonça <robsonmwoc@gmail.com>
2012-10-06 16:37:28 -0300

    Moving the external folter to tools

--- lib/supido.rb
+++ lib/supido.rb
@@ -8,7 +8,7 @@ module Supido; end
 
 # load Supido components
 require 'supido/abstract_benchmark_base'
-require 'supido/external/apache_benchmark'
+require 'supido/tools/apache_benchmark'
 require 'supido/config'
 require 'supido/profile_action'
 require 'supido/profile'
