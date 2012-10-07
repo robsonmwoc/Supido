019bd0736a416c390d7de2988c753f146b29fa5c
Robson Mendonça <robsonmwoc@gmail.com>
2012-10-07 14:04:36 -0300

    Some improvements on the tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,22 @@ $:.unshift(File.dirname(__FILE__))
 
 require 'supido'
 require 'rspec/autorun'
+require 'fileutils'
+
+RSpec.configure do |config|
+  config.after(:suite) {
+    Dir.glob("/tmp/benchmark_*").each { |dir| FileUtils.rm_rf(dir) }
+  }
+end
 
 module Supido
   class SampleBenchmarkTool < AbstractBenchmarkBase; end
+end
+
+def set_default_configuration
+  Supido.configure do |config|
+    config.benchmark_tool = Supido::Tools::ApacheBenchmark
+    config.log_path = "/tmp"
+  end
 end
 
